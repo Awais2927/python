3138517157c7c0a1ad7d98d260f933729faa32ce
Awais2927 <164341608+Awais2927@users.noreply.github.com>
2024-03-21 16:18:50 +0500

    Add files via upload

---
+++ wether.py
@@ -0,0 +1,10 @@
+def main():
+    num = float(input("Enter a number: "))
+    if num > 0:
+        print("The number is positive.")
+    elif num < 0:
+        print("The number is negative.")
+    else:
+        print("The number is zero.")
+if __name__ == "__main__":
+    main()
